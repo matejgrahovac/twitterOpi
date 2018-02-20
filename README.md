3e9aba03ef4a22a20233af2f6ca9aa449bd64f10
Matej Grahovac <admin@matejgrahovac.de>
2018-02-20 23:11:37 +0100

    this-is-first-commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# twitterOpi
+
+Minimale Twitter Opinion Mining Web Application
+
+
+[flask](http://flask.pocoo.org/)
+
+[nltk](https://www.nltk.org/)
+
+[plotly](https://plot.ly/)
+
+[NLTK](https://www.nltk.org/)
+
+[twython](https://twython.readthedocs.io/en/latest/)
+
+[Wörterdatenbank SentiWS der Universität Leipzig](http://wortschatz.uni-leipzig.de)
+
+R. Remus, U. Quasthoff & G. Heyer: SentiWS - a Publicly Available German-language Resource for Sentiment Analysis.
+In: Proceedings of the 7th International Language Ressources and Evaluation (LREC'10), 2010
+
+![Index](https://github.com/matejgrahovac/twitterOpi/blob/master/screenshots/index.jpg?raw=true "Index")
+
+![Ergebnis](https://github.com/matejgrahovac/twitterOpi/blob/master/screenshots/papaponti.jpg?raw=true "Ergebnis")
---
+++ analyzer.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import nltk
+
+
+def cformat(wordlist):
+
+    newformat = {}
+
+    with open(wordlist) as words:
+        for line in words:
+            if line[0].isalnum():
+                line = line.rstrip("\n")
+                thisword = line.split()[0].split('|')[0]
+                POS = line.split()[0].split('|')[1]
+                polarity = line.split()[1]
+                try:
+                    wordforms = line.split()[2].split(',')
+                    for wordf in wordforms:
+                        newformat[wordf] = [POS, polarity]
+                except:
+                    pass
+
+                newformat[thisword] = [POS, polarity]
+            if 'EOF' in line:
+              break
+
+    return newformat
+
+
+
+class Analyzer():
+    """Implements sentiment analysis."""
+
+    def __init__(self, positives, negatives):
+        """Initialize Analyzer."""
+
+
+        self.posi = cformat(positives)
+        self.nega = cformat(negatives)
+
+
+    def analyze(self, text):
+        """Analyze text for sentiment, returning its score."""
+
+        # Converting uppercase letters to lowercase letters and stripping whitespace.
+        text = text.strip()
+
+        # Splitting sentence in words.
+        tokenizer = nltk.tokenize.TweetTokenizer()
+        tokens = tokenizer.tokenize(text)
+
+        score = 0.0
+
+        for tokenword in tokens:
+            try:
+                self.posi[tokenword]
+                score += float(self.posi[tokenword][1])
+            except:
+                try:
+                    self.nega[tokenword]
+                    score += float(self.nega[tokenword][1])
+                except:
+                    score += 0
+
+        return score
